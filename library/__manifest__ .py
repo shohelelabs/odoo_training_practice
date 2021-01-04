18448690baaa2470e8ea2a1a52804efbf64993ca
shohelelabs <shohel.mahmud@elabs.de>
2021-01-04 12:54:14 +0000

    Add Module

---
+++ library/__manifest__ .py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'Summary':"""Academy app to manage Training""",
+    'description':"""
+        Academy module to manage Training:
+        - Course
+        - Sessions
+        - Attendees
+    """,
+    'author':'Elabs',
+    'website':'https://www.elabs.de',
+    'category': 'Training',
+    'version':'0.1',
+    'depends':['base'],
+    'data':[
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
